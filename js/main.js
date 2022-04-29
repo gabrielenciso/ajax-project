d2f9de38f3f82eceb738d99c9f2809094e13e7f1
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-04-29 21:56:13 +0000

    added friends select options when window is loaded

--- js/main.js
+++ js/main.js
@@ -243,8 +243,10 @@ function handleScreenChange(event) {
   if (!event.matches) {
     $entriesList.style.height = '';
     mobile = false;
+    console.log(event);
   } else {
     mobile = true;
+    console.log(event);
   }
 }
 
@@ -364,6 +366,13 @@ function handleLoadEntry(event) {
       makeMarker(currEntry.marker.latLng, map, currEntry.fromFriend.photo, currEntry.entryId);
     }
   }
+
+  if (data.friends !== []) {
+    for (var j = 0; j < data.friends.length; j++) {
+      var currentFriend = data.friends[j];
+      addFriendSelectOption(currentFriend);
+    }
+  }
 }
 
 if (data.entries.length !== 0) {
@@ -477,6 +486,9 @@ function deleteEntry(dataId) {
 }
 
 function focusEntry(dataId) {
+  if (mobile === true) {
+    $entriesList.style.height = 150 + 'px';
+  }
   var targetRec = document.querySelector('li[data-entry-id="' + dataId + '"]');
   targetRec.scrollIntoView({ behavior: 'smooth', block: 'center' });
 }
